1449e62c7e478027b24398d994cb125e736dbabf
John Howard <longhornjoho@gmail.com>
2016-11-06 00:37:21 -0500

    refactored to use API; extended service to al post, put and delete; created search ability

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,32 +1,39 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroService }         from './hero.service';
+import { AppRoutingModule } from './app-routing.module';
 
-import { AppRoutingModule }   from './app-routing.module';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
+import { HeroSearchComponent } from './hero-search.component'
+
+import './rxjs-extensions';
 
 @NgModule({
-  imports: [ 
+  imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
-  declarations: [ 
+  declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
-    DashboardComponent
+    HeroSearchComponent
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [ 
-    AppComponent
-  ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
